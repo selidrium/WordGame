41b0b47c98d0f64f28a4d918ecad98ae988af8ab
Alan Garcia <alangarcia1996@gmail.com>
2023-03-22 20:42:37 -0700

    completed lesson 2

---
+++ src/Gameplay.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Gameplay {
+
+    private static Person person;
+    public static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        // Collect User Info
+        System.out.println("What is your first name?");
+        String firstName = scan.next();
+        System.out.println("You are entering a last name? (y/n)");
+        String answer = scan.next().toLowerCase();
+        if (answer.equals("y")){
+            System.out.println("Enter your last name");
+            String lastName = scan.next();
+            person = new Person(firstName, lastName);
+        } else {
+            person = new Person(firstName);
+        }
+
+        // Game starts
+        Numbers game = new Numbers();
+        game.generateNumber();
+        boolean guessed = false;
+        while ( !guessed ) {
+            System.out.println("Guess a number between 1 - 100");
+            int guess = scan.nextInt();
+            if (game.compareNumber(guess)) {
+                guessed = true;
+            }
+
+        }
+
+    }
+}
---
+++ src/Numbers.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+
+public class Numbers {
+
+    private int randomNum;
+
+    // Setters and Getters
+    public int getRandomNum() {
+        return randomNum;
+    }
+
+    public void setRandomNum(int randomNum) {
+        this.randomNum = randomNum;
+    }
+
+    // Methods
+    public void generateNumber() {
+        Random rand = new Random();
+        randomNum = rand.nextInt(101);
+    }
+
+    public boolean compareNumber(int guess) {
+        if (guess == randomNum) {
+            System.out.println("Congratulations, you guessed the number!");
+            return true;
+        }
+        else if ( guess > randomNum) {
+            System.out.println("I'm sorry.  That guess was too high.");
+            return false;
+        }
+        else {
+            System.out.println("I'm sorry, That guess was too low.");
+            return false;
+        }
+
+    }
+
+
+
+}
