ac309174e1219be2a2fde39915669385d9bf24cd
Alan Garcia <alangarcia1996@gmail.com>
2023-04-18 16:02:34 -0700

    Lesson 10, updated GUI, loaded sounds and images

--- src/GUI.java
+++ src/GUI.java
@@ -1,5 +1,10 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.sound.sampled.*;
 import javax.swing.*;
 import javax.swing.text.DefaultCaret;
 
@@ -18,12 +23,25 @@ public class GUI extends JFrame implements ActionListener {
     private int index = 0;
     private JMenuBar menuBar;
     private JMenu gameMenu, aboutMenu;
+    private BufferedImage prizeImage;
+    private JLabel prizeImageLabel;
+    private JMenuItem imageAttributionItem, soundAttributionItem;
+    private Clip correctAnswerClip, incorrectAnswerClip;
+    private String imageAttributionText = "Image attribution text";
+
 
     public GUI() {
-        setTitle("Game Show");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setPreferredSize(new Dimension(1000, 700));
-        setLayout(null);
+
+        // (Load image)
+        try {
+            prizeImage = ImageIO.read(new File("media/images/prize.jpeg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // (Load sounds)
+        correctAnswerClip = loadSound("media/sounds/sound2.wav");
+        incorrectAnswerClip = loadSound("media/sounds/sound2.wav");
 
         // Add the menu bar
         menuBar = new JMenuBar();
@@ -33,6 +51,40 @@ public class GUI extends JFrame implements ActionListener {
         gameMenu = new JMenu("Game");
         gameMenu.setMnemonic(KeyEvent.VK_G);
         menuBar.add(gameMenu);
+
+        // Add the about menu
+        aboutMenu = new JMenu("About");
+        aboutMenu.setMnemonic(KeyEvent.VK_A);
+        menuBar.add(aboutMenu);
+
+        // (Add image and sound attribution menu items)
+        imageAttributionItem = new JMenuItem("Image Attribution");
+        imageAttributionItem.addActionListener(this);
+        aboutMenu.add(imageAttributionItem);
+
+        soundAttributionItem = new JMenuItem("Sound Attribution");
+        soundAttributionItem.addActionListener(this);
+        aboutMenu.add(soundAttributionItem);
+
+        // (Add prize image label)
+        prizeImageLabel = new JLabel(new ImageIcon(prizeImage));
+        prizeImageLabel.setBounds(500, 100, prizeImage.getWidth(), prizeImage.getHeight());
+        add(prizeImageLabel);
+        prizeImageLabel.setVisible(false);
+
+        // (Add animation)
+        Thread animationThread = new Thread(() -> {
+            while (true) {
+                animateImage();
+            }
+        });
+        animationThread.start();
+
+        setTitle("Game Show");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(1000, 700));
+        setLayout(null);
+
 
         // Add the add player menu item
         addPlayerItem = new JMenuItem("Add Player");
@@ -44,10 +96,6 @@ public class GUI extends JFrame implements ActionListener {
         addHostItem.addActionListener(this);
         gameMenu.add(addHostItem);
 
-        // Add the about menu
-        aboutMenu = new JMenu("About");
-        aboutMenu.setMnemonic(KeyEvent.VK_A);
-        menuBar.add(aboutMenu);
 
         // Add the layout menu item
         layoutItem = new JMenuItem("Layout");
@@ -94,7 +142,75 @@ public class GUI extends JFrame implements ActionListener {
         setVisible(true);
     }
 
+    public void showPrizeImage() {
+        prizeImageLabel.setVisible(true);
+    }
+
+    // (Load sound method)
+    private Clip loadSound(String path) {
+        try {
+            File soundFile = new File(path);
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+            clip.loop(0);
+            return clip;
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    // (Animate image method)
+    private void animateImage() {
+        try {
+            int x = 0;
+            while (x < this.getWidth()) {
+                prizeImageLabel.setLocation(x, prizeImageLabel.getY());
+                x += 2;
+                Thread.sleep(30);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void actionPerformed(ActionEvent e) {
+
+        // (Handle new menu item actions)
+        if (e.getSource() == imageAttributionItem) {
+            // Show image attribution dialog
+            new AttributionDialog("Image Attribution", imageAttributionText);
+        }
+
+        // (Handle new menu item actions)
+        if (e.getSource() == imageAttributionItem) {
+            // Show image attribution dialog
+            new AttributionDialog("Image Attribution", "Image 1");
+        } else if (e.getSource() == soundAttributionItem) {
+            // Show sound attribution dialog
+            new AttributionDialog("Sound Attribution", "Sound 1");
+        }
+
+        if (win) {
+            prizeImageLabel.setVisible(true);
+            int choice = JOptionPane.showConfirmDialog(null, "Do you want to play again?", "Play Again",
+                    JOptionPane.YES_NO_OPTION);
+            if (choice == JOptionPane.YES_OPTION) {
+                loadSound("media/sounds/sound1.mp3");
+
+                showPrizeImage();
+                animateImage();
+                ImageDisplay.displayImage("media/images/prize.jpeg");
+
+
+                resetGame();
+            } else {
+                System.exit(0);
+            }
+        }
+
+
         if (e.getSource() == addPlayerItem ) {
             if (playerIndexCounter == 3) {
                 JOptionPane.showMessageDialog(null, "Only three players are allowed.");
@@ -136,6 +252,30 @@ public class GUI extends JFrame implements ActionListener {
         }
     }
 
+    // (New AttributionDialog class)
+    static class AttributionDialog extends JDialog {
+        public AttributionDialog(String title, String attributionText) {
+            setTitle(title);
+            setSize(300, 200);
+            setLocationRelativeTo(null);
+            setLayout(new BorderLayout());
+
+            JTextArea textArea = new JTextArea(attributionText);
+            textArea.setEditable(false);
+            textArea.setLineWrap(true);
+            textArea.setWrapStyleWord(true);
+            JScrollPane scrollPane = new JScrollPane(textArea);
+            scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+            add(scrollPane, BorderLayout.CENTER);
+
+            JButton closeButton = new JButton("Close");
+            closeButton.addActionListener(e -> dispose());
+            add(closeButton, BorderLayout.SOUTH);
+
+            setVisible(true);
+        }
+    }
+
     public void updatePlayersLabel(String playerName) {
         if (playerIndexCounter == 1) {
             player1Label.setText("Players 01: " + playerName);
@@ -145,6 +285,8 @@ public class GUI extends JFrame implements ActionListener {
             player3Label.setText("Players 03: " + playerName);
         }
     }
+
+
 
     public void resetGame() {
         if(!saveMessagesItem.isSelected())
@@ -161,8 +303,29 @@ public class GUI extends JFrame implements ActionListener {
         }
     }
 
-    public static void main(String[] args) {
-        GUI gameShow = new GUI();
+    public class ImageDisplay {
+
+        public static void displayImage(String imagePath) {
+            // Create a new JFrame
+            JFrame frame = new JFrame("Image Display");
+
+            // Create a new JLabel to hold the image
+            JLabel imageLabel = new JLabel();
+
+            // Load the image from the file
+            ImageIcon imageIcon = new ImageIcon(imagePath);
+
+            // Set the image on the JLabel
+            imageLabel.setIcon(imageIcon);
+
+            // Add the JLabel to the JFrame
+            frame.add(imageLabel);
+
+            // Set the JFrame size and visibility
+            frame.setSize(imageIcon.getIconWidth(), imageIcon.getIconHeight());
+            frame.setVisible(true);
+        }
     }
+
 
 }
